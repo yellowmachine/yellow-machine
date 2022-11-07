e679535f641538ac3b93acc4c96cbdb373986d21
yellow <yellow.machine@mailfence.com>
2022-11-07 21:40:32 +0100

    run function

--- src/index.ts
+++ src/index.ts
@@ -24,20 +24,25 @@ export function watch(options: {f: ((arg0: (()=>void)) => void), quit: string, f
         watcher.close();
     }
 
-    const watcher = chwatch(options.files, {ignoreInitial: true}).
-        on('all', async (event, path) => {
-        // eslint-disable-next-line no-console
-        console.log(event, path);
+    async function run(f: ((arg0: (()=>void)) => void)){
         try{
-            await options.f(close);
+            await f(close);
             // eslint-disable-next-line no-console
             console.log("Press " + options.quit + " to quit.");
         }catch(err){
             // eslint-disable-next-line no-console
             console.log(err);
         }
-        
+    }
+
+    const watcher = chwatch(options.files, {ignoreInitial: true}).
+        on('all', (event, path) => {
+        // eslint-disable-next-line no-console
+        console.log(event, path);
+        run(options.f);
     });
+
+    run(options.f);
 
     return p;
 }
@@ -48,56 +53,14 @@ export async function pipe(...rest: (((arg0: any) => any)|string)[]){
 
     try{
         for(const t of rest){
-            if(typeof t === 'function')
+            if(typeof t === 'function'){
                 data = await t(data);
+            }
         }
         ok = true;
     }catch(err){
         if([...rest].at(-1) === 'throws')
             throw err;
     }
-    
     return ok;
 }
-
-/*
-function f1(){
-    //throw new Error('salimos??')
-    console.log('f1')
-}
-
-function f2(){
-    console.log('f2')
-}
-
-function mydocker(){
-    let ctx = null
-    return {
-        up: async () => {
-            console.log('up')
-            ctx = 1
-        },
-        down: async () => {
-            console.log('down', ctx)
-        }
-    }
-}
-
-const {up, down} = mydocker()
-
-async function main(){
-    let ok = await pipe(up, 'throws')
-
-    if(ok){
-        const {listen, close} = watch({files: ["*.js"], quit: 'q'})
-        while(await listen()){
-            ok = await pipe(f1, f2) 
-            if(!ok)   
-                close()
-        }
-        await pipe(down)
-    }
-}
-
-main()
-*/
