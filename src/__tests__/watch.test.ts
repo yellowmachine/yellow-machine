20265c2255617b2f0a51d37c177cb56dedeccb0a
yellow <yellow.machine@mailfence.com>
2022-11-08 12:17:29 +0100

    added watch test for testing when a file changes

--- src/__tests__/watch.test.ts
+++ src/__tests__/watch.test.ts
@@ -1,3 +1,4 @@
+import {openSync, close, writeSync} from 'fs';
 import { watch, pipe } from '../index';
 
 async function f1(){
@@ -23,3 +24,32 @@ test('watch', async () => {
     const v = await f();
     expect(v).toBeTruthy();
   });
+
+test('watch 2', async () => {
+    let count = 0;
+
+    async function f_count(){
+        count += 1;
+    }
+    const fileName = "./src/__tests__/a.hey";
+    const interval = setInterval(()=>{
+        const fh = openSync(fileName, 'a');
+        writeSync(fh, ""+count);
+        close(fh);
+    }, 1000);
+
+    async function f(){
+        await watch({files: [fileName], 
+                     quit: 'q', 
+                     f: async (quit)=>{
+            await pipe(f_count);
+            if(count === 3)   
+                quit();
+            }}
+        );
+        return true; 
+    }
+    await f();
+    expect(count).toBe(3);
+    clearInterval(interval);
+});
