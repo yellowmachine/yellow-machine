c495268bdf84b02ea677492e6d5594ca499c7a42
yellow <yellow.machine@mailfence.com>
2022-11-15 10:52:58 +0100

    remove test file

--- src/__tests__/qkeyevent.test.ts
+++ src/__tests__/qkeyevent.test.ts
@@ -1,4 +1,5 @@
 import { context as C } from '../index';
+import watch from '../watch';
 
 test("quiting w with keypress q", async ()=> {
     const path: string[] = [];
@@ -9,7 +10,21 @@ test("quiting w with keypress q", async ()=> {
         process.stdin.emit("keypress", 'q');
     };
     
-    const {serial, w} = C();
-    await serial([a, [w(["*.hey"], [b]), end]]);
+    const {serial, w} = C({}, {w: watch(["*.js"])});
+    await serial([a, [w([b]), end]])();
+    expect(path).toEqual(['a', 'b', 'end']);
+});
+
+test("quiting w with keypress q compact mode", async ()=> {
+    const path: string[] = [];
+    const a = async() => path.push('a');
+    const end = async() => path.push('end');
+    const b = async() => {
+        path.push('b');
+        process.stdin.emit("keypress", 'q');
+    };
+    
+    const {serial} = C({a, b, end}, {w: watch(["*.js"])});
+    await serial("a|w[b]end")(); 
     expect(path).toEqual(['a', 'b', 'end']);
 });
