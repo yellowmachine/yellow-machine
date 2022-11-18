117aa56fac60a8ed6a3a180cef3d8035aaec84c1
yellow <yellow.machine@mailfence.com>
2022-11-18 08:17:56 +0100

    passed r2[buffer[a|b"

--- src/nr.ts
+++ src/nr.ts
@@ -40,21 +40,42 @@ export default (mode: MODE = "nobuffer", bfunc: BFUNC = null) => (setup: SETUP):
                 console.log('buffer push');
                 const x = createResolve();
                 buffer.push({...x, data});
-                await x.promise; 
+                try{
+                    await x.promise; 
+                }catch(err){
+                    console.log(err);
+                    if(buffer.length > 0){
+                        console.log('pop reject cascade');
+                        const {reject} = buffer.pop() as BufferData;
+                        if(reject) reject();
+                    }
+                    throw err;        
+                }
+                
             }else{
                 return false;
             }
         }
-        console.log('exited');
-        exited = false;
-        await pipe(data);
-        exited = true;
-        if(buffer.length > 0){
-            console.log('pop');
-            const {resolve} = buffer.pop() as BufferData;
-            if(resolve) resolve(true);
+        try{
+            console.log('exited');
+            exited = false;
+            await pipe(data);
+            exited = true;
+            if(buffer.length > 0){
+                console.log('pop resolve');
+                const {resolve} = buffer.pop() as BufferData;
+                if(resolve) resolve(true);
+            }
+            return true;
+        }catch(err){
+            console.log(err);
+            if(buffer.length > 0){
+                console.log('pop reject');
+                const {reject} = buffer.pop() as BufferData;
+                if(reject) reject();
+            }
+            throw err;
         }
-        return true;
     };
 
     return g;
