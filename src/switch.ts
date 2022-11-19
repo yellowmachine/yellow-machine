1cd828789372ef4ea4a6369287397f827f51072d
yellow <yellow.machine@mailfence.com>
2022-11-19 07:25:31 +0100

    switch can return number|boolean number to index pipes boolean to run given pipe or not

--- src/switch.ts
+++ src/switch.ts
@@ -1,9 +1,15 @@
-///
 import { Data, type SETUP } from '.';
-type SWF = (data: any)=>number;
+type SWF = (data: any)=>number|boolean;
 
 export default (f: SWF) => (setup: SETUP) => async (data: Data) => {
+    const pipe = setup["single"];
     const pipes = setup["multiple"];
-    const pipe = pipes[f(data)];
-    return await pipe(data);    
+
+    const v = f(data);
+    if(typeof v === 'boolean'){
+        if(v) return await pipe(data);
+        else return null;
+    }else{
+        return await pipes[v](data); 
+    }
 };
